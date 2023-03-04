53b722871e7666a4ad93ad62dce43cfbc2c55af3
Ezequiel Gómez Merlo <gomezmerloe@gmail.com>
2023-03-04 13:42:02 -0300

    modified cors with white list

--- api/src/app.js
+++ api/src/app.js
@@ -11,25 +11,39 @@ const server = express();
 
 server.name = "API";
 
+const whiteList = ["https://henry-food-one.vercel.app"];
+
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
-server.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://henry-food-one.vercel.app"
-  ); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
+server.use(cors());
 
-server.use("/", routes);
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whiteList.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+// server.use((req, res, next) => {
+//   res.header(
+//     "Access-Control-Allow-Origin",
+//     "https://henry-food-one.vercel.app"
+//   ); // update to match the domain you will make the request from
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+//   next();
+// });
+
+server.use("/", cors(corsOptions), routes);
 
 // Error catching endware.
 server.use((err, req, res, next) => {
