6483e2496288395c8dae41aed8afd840d89d9c13
EzeGomez0407 <gomezmerloe@gmail.com>
2026-01-20 12:08:15 -0300

    cambio en el titulo de la pagina y el landingpage

--- client/src/Components/LandingPage/LandingPage.js
+++ client/src/Components/LandingPage/LandingPage.js
@@ -6,12 +6,13 @@ import { NavLink } from "react-router-dom";
 const LandingPage = (props) => {
   return (
     <div className="landing-page">
-      <h1 className="titleLanding">Bienvenido a Henry Food</h1>
+      <h1 className="titleLanding">Las Recetas de Ratuille</h1>
       <div className="contentBtn">
         <NavLink to="/recipes" className="btnGetHome">
           Ver Recetas <BiRightArrow />
         </NavLink>
       </div>
+      <img src="/ratuile.png" alt="imagen de ratuile sosteniendo un plato" className="img-ratuile"/>
     </div>
   );
 };
